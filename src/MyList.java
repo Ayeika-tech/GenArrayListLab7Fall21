1a9bdb2a6f08c1f3994a65b79cbdd79f3a87e18e
ayeika-tech <2015muslimah@gmail.com>
2021-10-26 12:16:29 -0500

    Added printed numbers

--- src/MyList.java
+++ src/MyList.java
@@ -13,7 +13,9 @@ public class MyList<T extends Number> {
     public void add(T e) {
         arrayList.add(e);
     }
-
+    //largest method will loop thru arraylist and  compare the value of index 0 to index 1
+    // if i[1] is bigger then the value gets stored on lValue, then goes next index[2] and compares to 
+    //lValue, if index[2] if larger then holds at lValue and gets compared to i[3] and so forth.
     public T largest() {
         T lValue = arrayList.get(0);
         for (int i = 1; i < arrayList.size(); i++) {
@@ -23,7 +25,9 @@ public class MyList<T extends Number> {
         }
         return lValue;
     }
-
+ //smallest method will loop thru arraylist and  compare the value of index 0 to index 1
+    // if i[1] is smaller, then the value gets stored on sValue, then goes next index[2] and compares to 
+    //sValue, if index[2] is smaller then its set to sValue and gets compared to i[3] and so forth.
     public T smallest() {
         T sValue = arrayList.get(0);
         for (int i = 1; i < arrayList.size(); i++) {
--- src/MyListGeneric.java
+++ src/MyListGeneric.java
@@ -1,25 +1,32 @@
-
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class MyListGeneric {
     public static void main(String[] args) throws Exception {
       
       
-
+      //Initializing an Object of MyList of a Double type
       MyList<Double> doubleList = new MyList<Double>();
         doubleList.add(187.21);
         doubleList.add(2322.12);
         doubleList.add(10.99);
         doubleList.add(1907.82);
-
+        
+        //Initializing an Object of MyList of an Integer type
         MyList<Integer> intList = new MyList<Integer>();
         intList.add(120);
         intList.add(3865);
         intList.add(76);
         intList.add(112);
-
+      
+        for (Double list : doubleList.arrayList) {
+        System.out.printf("%5.2f%n", list);
+      }
         System.out.println("The largest double element is : "+doubleList.largest());
         System.out.println("The smallest double element is: "+doubleList.smallest()+"\n");
-       
+        for (Integer list : intList.arrayList) {
+          System.out.printf("%d%n", list);
+        }
         System.out.println("The largest int element is : "+intList.largest());
         System.out.println("The smallest int element is: "+intList.smallest());
     }
